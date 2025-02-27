4f2787b63948557a364ae1be1a6648684ceced45
Daffa Radifanka <daffa.radifanka@dretail.id>
2025-02-27 14:45:21 +0700

    feat new dashboard

--- src/routes/dashboardRoutes.js
+++ src/routes/dashboardRoutes.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { getDashboardMurabbi, getDashboardTholib } = require("../controllers/dashboardController"); // ✅ Pastikan import benar
+const { getDashboardMurabbi, getDashboardTholib, getDashboardMurabbiReported, getDashboardMurabbiUnreported } = require("../controllers/dashboardController"); // ✅ Pastikan import benar
 const { verifyToken, checkMurabbi, checkTholib } = require("../middlewares/authMiddleware");
 
 
@@ -7,5 +7,6 @@ const router = express.Router();
 
 router.get("/murabbi", verifyToken, checkMurabbi, getDashboardMurabbi);
 router.get("/tholib", verifyToken, checkTholib, getDashboardTholib);
-
+router.get("/murabbi/reported", verifyToken, checkMurabbi, getDashboardMurabbiReported)
+router.get("/murabbi/unreported", verifyToken, checkMurabbi, getDashboardMurabbiUnreported)
 module.exports = router;
